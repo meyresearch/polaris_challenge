d7a1a1a9b5ad539b84641dae8b90e6c7310e3b0c
Lex O'brien <lex.e.obrien@gmail.com>
2025-02-23 21:57:30 +0000

    performed imputations in R

---
+++ admet/utils.py
@@ -0,0 +1,32 @@
+
+from stepmix.stepmix import StepMix
+import seaborn as sns
+import numpy as np
+
+def missing_data_clusters(df, ignore=['CXSMILES', 'Molecule Name']): 
+    df = df.copy(deep=True)
+    for col in df.columns.difference(ignore): 
+        df[f'isna-{col}'] = df[col].isna()
+    clusters = [3, 4, 5, 6, 7, 8, 9, 10]
+    for i in clusters:
+        model = StepMix(n_components=i, measurement='binary', verbose=0, random_state=42)
+        model.fit(df.filter(regex='^isna'))
+        df[f'class_n{i}'] = model.predict(df.filter(regex='^isna'))
+    return df
+
+def plot_heatmap(df, n_clusters, ax): 
+    ccol = f"class_n{n_clusters}"
+    to_plot = df.filter(regex=f'(^isna)|({ccol})').copy()
+
+    to_plot = to_plot.sort_values(by=ccol, ascending=True)
+    to_plot[ccol] += 1
+
+    for col in to_plot.columns.difference([ccol]):
+        to_plot[col] = to_plot.apply(lambda x: {True: x[ccol],False: np.nan}[x[col]], axis=1)
+    
+    return sns.heatmap(to_plot, cmap=sns.color_palette('colorblind', n_clusters+1),  square=False, cbar=True, ax=ax)
+
+
+
+
+
